4d51480a9025ce20862b8c83f18e646deaa7962b
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-23 03:38:51 +0400

    First plugin config file

---
+++ lib/config/plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is a part of "RSS Feed of Products" plugin for ShopScript 5
+ * 
+ * @author Serge Rodovnichenko <sergerod@gmail.com>
+ *
+ * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
+ * @version 1.0.0
+ */
+
+return array(
+    'name' => 'RSS Feed of products',
+//    'description' => 'More reports',
+    'vendor'=>670917,
+    'version'=>'1.0.0',
+    'shop_settings' => FALSE,
+    'frontend'    => TRUE,
+//    'icons'=>array(
+//        16 => 'img/actions-office-chart-line-percentage-icon.png'
+//        ),
+//    'handlers' => array(
+//        'backend_reports' => 'backendReports',
+//    ),
+    'locale' => array('en_US', 'ru_RU')
+);
