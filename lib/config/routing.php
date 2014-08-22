282ae8f0669de61b9459a37737dff0edee61ead3
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-23 03:35:23 +0400

    Start

---
+++ lib/config/routing.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is a part of "RSS Feed of Products" plugin for ShopScript 5
+ * 
+ * @author Serge Rodovnichenko <sergerod@gmail.com>
+ *
+ * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
+ * @version 1.0.0
+ */
+
+return array(
+    "rss/<hash:[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}>.xml" => array(
+        "plugin" => "syrrss",
+        "module" => "feed",
+        "action" => "catalog"
+    )
+);
