fe8ad2dfc8bcdc6f8dbd203ecc01bf4caaee68ec
Serge Rodovnichenko <sergerod@gmail.com>
2014-08-30 22:31:20 +0400

    settings definition file

---
+++ lib/config/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is a part of "RSS Feed of Products" plugin for ShopScript 5
+ *
+ * @author Serge Rodovnichenko <sergerod@gmail.com>
+ *
+ * @license http://www.webasyst.com/terms/#eula Webasyst Commercial
+ * @version 1.0.0
+ */
+
+return array();
