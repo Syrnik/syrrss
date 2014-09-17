108462c4c4a7c465a4471a07804757b5937844cd
Serge Rodovnichenko <sergerod@gmail.com>
2014-09-17 23:35:15 +0400

    Файл с системными требованиями

---
+++ lib/config/requirements.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    'php.dom' => array(
+        'strict' => TRUE,
+        'value' => 1
+    ),
+    'php.SimpleXML' => array(
+        'strict' => TRUE,
+        'value' => 1
+    ),
+    'php.xmlreader' => array(
+        'strict' => TRUE,
+        'value' => 1
+    ),
+    'php.xmlwriter' => array(
+        'strict' => TRUE,
+        'value' => 1
+    ),
+    'app.shop' => array(
+        'strict' => TRUE,
+        'version' => '5.2.0'
+    ),
+);
