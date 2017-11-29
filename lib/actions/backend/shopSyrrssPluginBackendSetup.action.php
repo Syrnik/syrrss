595016246505fdfb27a49493a26903ee9e9f3b0b
Serge Rodovnichenko <sergerod@gmail.com>
2017-11-29 21:40:01 +0300

    code reformat

--- lib/actions/backend/shopSyrrssPluginBackendSetup.action.php
+++ lib/actions/backend/shopSyrrssPluginBackendSetup.action.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of shopSyrrssPluginBackendSetup
  *
@@ -19,10 +13,10 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
 
     /** @var waRouting */
     private $Routing;
-    
+
     /** @var shopConfig */
     private $ShopConfig;
-    
+
     /** @var waAppSettingsModel */
     private $AppSettings;
 
@@ -46,7 +40,7 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
         $app_settings = array(
             'ignore_stock_count' => $this->AppSettings->get("shop", "ignore_stock_count", 0)
         );
-        
+
         $this->view->assign('primary_currency', $this->ShopConfig->getCurrency());
         $this->view->assign(compact("app_settings", "current_domain", "info", "profile", "profiles", "settlements"));
     }
@@ -62,8 +56,8 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
         $settlements = array();
         $domain_routes = $this->Routing->getByApp("shop");
 
-        foreach($domain_routes as $domain => $routes) {
-            foreach($routes as $route) {
+        foreach ($domain_routes as $domain => $routes) {
+            foreach ($routes as $route) {
                 $settlements[] = $domain . '/' . $route['url'];
             }
         }
@@ -79,15 +73,15 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
     {
         $profile = $this->Profile->getConfig();
         $profile["config"] += array(
-            "domain" => "",
-            "export_zero_stock" => 0,
-            "hash" => "",
-            "lifetime" => 0,
-            "max_products" => 15,
+            "domain"              => "",
+            "export_zero_stock"   => 0,
+            "hash"                => "",
+            "lifetime"            => 0,
+            "max_products"        => 15,
             "channel_description" => _wp("New products")
         );
-        
-        if(!isset($profile["config"]["channel_name"]) || empty($profile["config"]["channel_name"])) {
+
+        if (!isset($profile["config"]["channel_name"]) || empty($profile["config"]["channel_name"])) {
             $profile["config"]["channel_name"] = sprintf(_wp("Newest products in %s store"), $this->ShopConfig->getGeneralSettings("name"));
         }
 
@@ -98,11 +92,11 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
     {
         $domain_routes = $this->Routing->getByApp("shop");
 
-        foreach($domain_routes as $domain => $routes) {
-            foreach($routes as $route) {
+        foreach ($domain_routes as $domain => $routes) {
+            foreach ($routes as $route) {
                 $settlement = $domain . '/' . $route['url'];
 
-                if(($settlement == $current_domain) || ($current_domain === '')) {
+                if (($settlement == $current_domain) || ($current_domain === '')) {
                     $current_domain = $settlement;
                     $this->Routing->setRoute($route, $domain);
                     waRequest::setParam($route);
@@ -112,38 +106,38 @@ class shopSyrrssPluginBackendSetupAction extends waViewAction
 
         return $current_domain;
     }
-    
+
     /**
-     * 
+     *
      * @param array $profile массив с профилем
      * @return array Информация о файле с фидом
      */
     private function getXmlFileInfo($profile)
     {
-        $info = array('mtime' => NULL, 'exists' => NULL, "url" => NULL);
+        $info = array('mtime' => null, 'exists' => null, "url" => null);
 
-        if(!empty($profile["id"])) {
+        if (!empty($profile["id"])) {
             $feed_file = shopSyrrssPlugin::path("{$profile["id"]}.xml");
             $info["exists"] = file_exists($feed_file);
 
-            if($info["exists"]) {
+            if ($info["exists"]) {
                 $info["mtime"] = filemtime($feed_file);
-                $info["url"] = $this->Routing->getUrl("shop/frontend/feed", array("plugin" => shopSyrrssPlugin::PLUGIN_ID, "hash" => $this->plugin()->getHash($profile["id"])), TRUE);
+                $info["url"] = $this->Routing->getUrl("shop/frontend/feed", array("plugin" => shopSyrrssPlugin::PLUGIN_ID, "hash" => $this->plugin()->getHash($profile["id"])), true);
             }
         }
 
         return $info;
     }
-    
+
     /**
      * Singletons suck!
-     * 
+     *
      * @return shopSyrrssPlugin
      */
     private function plugin()
     {
         static $plugin;
-        if(!$plugin) {
+        if (!$plugin) {
             $plugin = wa()->getPlugin(shopSyrrssPlugin::PLUGIN_ID);
         }
         return $plugin;
